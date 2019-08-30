928fa71fdfe37256b48cd445d9f7142ac0b39cdf
Ismael <mael.dazareia@gmail.com>
2019-08-30 13:05:14 -0300

    Add project

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const nunjucks = require("nunjucks");
+
+const app = express();
+
+nunjucks.configure("./views", {
+    autoescape: true,
+    express: app,
+    watch: true
+});
+
+app.set("view engine", "njk");
+app.use(express.urlencoded({ extended: false }));
+
+const ageMiddleware = (req, res, next) =>
+    !req.query.age ? res.redirect("/") : next();
+
+app.get("/", (req, res) => res.render("age"));
+
+app.post("/check", (req, res) =>
+    req.body.age < 18
+        ? res.redirect(`/minor?age=${req.body.age}`)
+        : res.redirect(`/major?age=${req.body.age}`)
+);
+
+app.get("/major", ageMiddleware, (req, res) =>
+    res.render("major", { age: req.query.age })
+);
+
+app.get("/minor", ageMiddleware, (req, res) =>
+    res.render("minor", { age: req.query.age })
+);
+
+app.listen(3000);
